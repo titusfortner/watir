735c95c1f8ca267f3365bc68908d2801bb2f2abd
Titus Fortner <titusfortner@gmail.com>
2018-12-25 14:20:31 -0600

    remove support for toggling off relaxed_locate

--- spec/watirspec/alert_spec.rb
+++ spec/watirspec/alert_spec.rb
@@ -9,12 +9,10 @@ describe 'Alert API' do
     browser.alert.ok if browser.alert.exists?
   end
 
-  compliant_on :relaxed_locate do
-    context 'Relaxed Waits' do
-      context 'when acting on an alert' do
-        it 'raises exception after timing out' do
-          expect { browser.alert.text }.to wait_and_raise_unknown_object_exception
-        end
+  context 'Waits' do
+    context 'when acting on an alert' do
+      it 'raises exception after timing out' do
+        expect { browser.alert.text }.to wait_and_raise_unknown_object_exception
       end
     end
   end
@@ -54,47 +52,32 @@ describe 'Alert API' do
       end
     end
 
-    not_compliant_on :relaxed_locate do
-      describe 'wait_until_present' do
-        it 'waits until alert is present and goes on' do
-          browser.button(id: 'timeout-alert').click
-          browser.alert.wait_until_present.ok
-
-          expect(browser.alert).to_not exist
-        end
-
-        it 'raises error if alert is not present after timeout' do
-          expect { browser.alert.wait_until_present.ok }.to raise_timeout_exception
+    context 'confirm' do
+      describe '#ok' do
+        it 'accepts confirm' do
+          browser.button(id: 'confirm').click
+          browser.alert.ok
+          expect(browser.button(id: 'confirm').value).to eq 'true'
         end
       end
-    end
-  end
 
-  context 'confirm' do
-    describe '#ok' do
-      it 'accepts confirm' do
-        browser.button(id: 'confirm').click
-        browser.alert.ok
-        expect(browser.button(id: 'confirm').value).to eq 'true'
+      describe '#close' do
+        it 'cancels confirm' do
+          browser.button(id: 'confirm').click
+          browser.alert.close
+          expect(browser.button(id: 'confirm').value).to eq 'false'
+        end
       end
     end
 
-    describe '#close' do
-      it 'cancels confirm' do
-        browser.button(id: 'confirm').click
-        browser.alert.close
-        expect(browser.button(id: 'confirm').value).to eq 'false'
-      end
-    end
-  end
-
-  context 'prompt' do
-    describe '#set' do
-      it 'enters text to prompt' do
-        browser.button(id: 'prompt').click
-        browser.alert.set 'My Name'
-        browser.alert.ok
-        expect(browser.button(id: 'prompt').value).to eq 'My Name'
+    context 'prompt' do
+      describe '#set' do
+        it 'enters text to prompt' do
+          browser.button(id: 'prompt').click
+          browser.alert.set 'My Name'
+          browser.alert.ok
+          expect(browser.button(id: 'prompt').value).to eq 'My Name'
+        end
       end
     end
   end
